cc76022d3796b45033e6c7949dc858d6bf379adf
Sean Miller <millersm@whitman.edu>
2018-02-07 10:56:39 -0800

    Update wind.js

--- wind.js
+++ wind.js
@@ -1,3 +1,4 @@
+// Pull wind data for Davenport CA
 function addText(dir, spd, time, date, dcnt){
 	var month, day, str;
 	//assign direction
@@ -81,6 +82,7 @@ function addText(dir, spd, time, date, dcnt){
 	day = date.slice(3, 5);
 	date = month + " " + day;
 	str = spd + " mph " + dir + " wind at " + time + " on " + date;
+	//create and push element to DOM
 	var newEl = document.createElement('li');
 	var newText = document.createTextNode(str);
 	newEl.appendChild(newText);
@@ -88,28 +90,22 @@ function addText(dir, spd, time, date, dcnt){
 	position.append(newEl);
 	position2 = document.getElementById("d"+dcnt);
 	position2.innerHTML = date;
-	//alert(spd + " mph " + dir + " wind at " + time + " on " + date);
 }
 
-//alert("hello");
 function run(){
 	var requestURL = 'https://api.weather.gov/gridpoints/MTR/89,91'; // json data for Davenport CA
 	var request = new XMLHttpRequest();
 	request.open('GET', requestURL);
 	request.responseType = 'json';
 	request.send();
-	//alert("hello2");
 	request.onload = function() {
-		//alert("Inside onload");
 		var dir, spd, tod, dirCnt, spdCnt, date1, date2, time1, time2, daycount, startdate; // direction, magnitude, time of day, direction offset, speed offset, date1 & time1 are for DIRECTION;
 		var data = request.response;
 		dirCnt = 0;
 		spdCnt = 0;
-		//alert("before while loop");
 		daycount = 0;
 		startdate = data["properties"]["windDirection"]["values"][dirCnt]["validTime"].slice(5, 10);
 		while(dirCnt < data["properties"]["windDirection"]["values"].length && spdCnt < data["properties"]["windSpeed"]["values"].length){ // Only add data where speed and direction are temporally matched (there are instances where they lose sync)
-			//alert("inside while loop");
 			date1 = data["properties"]["windDirection"]["values"][dirCnt]["validTime"].slice(5, 10);
 			time1 = data["properties"]["windDirection"]["values"][dirCnt]["validTime"].slice(11, 13);
 			date2 = data["properties"]["windSpeed"]["values"][spdCnt]["validTime"].slice(5, 10);
@@ -124,7 +120,7 @@ function run(){
 					spdCnt++;
 					dirCnt++;
 				}
-				else if(time1 > time2){ // direction skipped and entry, update spdCnt
+				else if(time1 > time2){ // direction skipped, update spdCnt
 					spdCnt ++;
 				}
 				else{
